99ba7473e4996a9dc5f5cfad7f0f8c2ab115f2b5
@rgem52 <rgomez@hi-iberia.es>
2015-05-25 15:53:31 +0200

    //The template life-cycle callbacks

--- oi.js
+++ oi.js
@@ -60,6 +60,65 @@ if (Meteor.isClient) {
             helper: 'clone'
         });
     };
+
+    //Events & //The template life-cycle callbacks
+    Template.container.events({
+        'click button': function (event, template) {
+            //#A This stops the event from bubbling up the DOM.
+            event.stopImmediatePropagation();
+            //Template.instance() contains an object that represents the current template
+            var templateInstance = this;
+
+            var _body = $('body');
+            var colorR = Math.floor((Math.random() * 255) + 1);
+            var colorG = Math.floor((Math.random() * 255) + 1);
+            var colorB = Math.floor((Math.random() * 255) + 1);
+            var rgb = 'rgb' + '(' + colorR + ',' + colorG + ',' + colorB + ')';
+            _body.css("background-color", rgb);
+
+            //The template life-cycle callbacks
+            Template.instance().lastCallback = 'rendered and clicked';
+            console.log('container.clicked', this);
+            console.log('container.clicked.tplInstance', template);
+        }
+    });
+
+
+    //The template life-cycle callbacks
+    Template.container.created = function () {
+        // #A Prints out the instance of the profile template.
+        // You can set variables to the template instance like this.foo = 'bar' that you could use later.
+        // You can also read the data context, but you can’t set it here.
+        this.lastCallback = 'created';
+        console.log('container.created', this);
+    };
+    Template.container.rendered = function () {
+        // #A Prints out the instance of the profile template.
+        // You can set variables to the template instance like this.foo = 'bar' that you could use later.
+        // You can also read the data context, but you can’t set it here.
+        this.lastCallback = 'rendered';
+        console.log('container.rendered', this);
+    };
+    Template.container.destroyed = function () {
+        // #A Prints out the instance of the profile template.
+        // You can set variables to the template instance like this.foo = 'bar' that you could use later.
+        // You can also read the data context, but you can’t set it here.
+        this.lastCallback = 'destroyed';
+        console.log('container.destroyed', this);
+    };
+    Template.container.helpers({
+        placeholder: function () {
+            //#B Prints out only the data context.
+            // You have no access to the template instance via this.
+            console.log('container.placeholder', this);
+            //#C You can still access the template instance in a template helpers and events.
+            console.log('container.tplInstance', Template.instance().lastCallback);
+            //In the event handlers you don’t need Template.instance()
+            // because the template instance is passed as the second parameter directly.
+            return 'This is the {{placeholder}} helper';
+        }
+    });
+
 }
 
 if (Meteor.isServer) {
